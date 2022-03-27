4ce2e6360ba1392cefa8dc687fd1fd5fe86b42bb
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-03-27 09:40:45 -0500

    final

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state, action) {
+	switch (action.type) {
+		case 'deposit':
+			return state + action.payload;
+		case 'withdraw':
+			return state - action.payload;
+		default:
+			return state;
+	}
 }
 
-export default App;
+export default function ComponentWithUseReducer() {
+	const deposit = (amount) => {
+		dispatch({
+			type: 'deposit',
+			payload: amount,
+		});
+	};
+
+	const withdraw = (amount) => {
+		dispatch({
+			type: 'withdraw',
+			payload: amount,
+		});
+	};
+
+	const [amount, dispatch] = useReducer(
+		reducer,
+		0
+	);
+
+	return (
+		<div>
+			<h1>{amount}</h1>
+			<button onClick={() => deposit(500)}>
+				Deposit
+			</button>
+			<button onClick={() => withdraw(500)}>
+				Withdraw
+			</button>
+		</div>
+	);
+}
